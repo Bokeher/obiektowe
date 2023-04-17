f042ea971cb5d39ec369f4d2297daf29bfbc251f
Bokeher <72520841+Bokeher@users.noreply.github.com>
2023-04-17 14:36:17 +0200

    kolko krzyzyk

--- 6.0_gra_kolko_krzyzyk/Board.java
+++ 6.0_gra_kolko_krzyzyk/Board.java
@@ -1,15 +1,18 @@
 public class Board {
     private String[] board = {" ", " ", " ", " ", " ", " ", " ", " ", " "}; 
+    private String winnerSign = "";
 
     @Override
     public String toString() {
-        String res = board[0]+" | "+board[1]+" | "+board[2]+"\n"+
-                     "----------\n"+
-                     board[3]+" | "+board[4]+" | "+board[5]+"\n"+
-                     "----------\n"+
-                     board[6]+" | "+board[7]+" | "+board[8]+"\n";
+        String res = "  | a | b | c \n"+
+                     "--------------\n"+
+                     "1 | "+board[0]+" | "+board[1]+" | "+board[2]+"\n"+
+                     "--------------\n"+
+                     "2 | "+board[3]+" | "+board[4]+" | "+board[5]+"\n"+
+                     "--------------\n"+
+                     "3 | "+board[6]+" | "+board[7]+" | "+board[8]+"\n";
         return res;
-    } 
+    }
 
     /**
      * Puts sign in index
@@ -20,9 +23,57 @@ public class Board {
     public boolean setSignAt(int index, String sign) {
         if(!sign.equals("X") && !sign.equals("O")) return false;
         if(index < 0 || index > 8) return false;
-
+        if(!board[index].equals(" ")) return false; 
         board[index] = sign;
 
         return true;
+    }
+
+    public String getSignAt(int index) {
+        if(index > 8 && index < 0) return null;
+        return board[index];
+    }
+
+    public boolean checkWin() {
+        // check rows
+        if(checkLine(0, 1, 2)) return true;
+        if(checkLine(3, 4, 5)) return true;
+        if(checkLine(6, 7, 8)) return true;
+
+        //check colums
+        if(checkLine(0, 3, 6)) return true;
+        if(checkLine(1, 4, 7)) return true;
+        if(checkLine(2, 5, 8)) return true;
+
+        //check diagonals
+        if(checkLine(0, 4, 8)) return true;
+        if(checkLine(2, 4, 6)) return true;
+
+        if(checkDraw()) return true;
+
+        return false;
+    }
+    private boolean checkLine(int a, int b, int c) {
+        if(board[a].equals(" ") || board[b].equals(" ") || board[c].equals(" ")) return false;
+        
+        if(board[a].equals(board[b]) && board[b].equals(board[c])) {
+            winnerSign = board[a];
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkDraw() {
+        for (String place : board) {
+            if(place.equals(" ")) {
+                winnerSign = "-";
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public String getWinnerSign() {
+        return winnerSign;
     }
 }
--- 6.0_gra_kolko_krzyzyk/ServerMain.java
+++ 6.0_gra_kolko_krzyzyk/ServerMain.java
@@ -1,12 +1,16 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ServerMain {
+    static Board board;
     public static void main(String[] args) {
         try {
             ServerSocket server = new ServerSocket(23459);
@@ -15,20 +19,126 @@ public class ServerMain {
             DataInputStream in = new DataInputStream(socket.getInputStream());
 
             Scanner sc = new Scanner(System.in);
+            File f = new File("gameHistory.txt");
 
-            Board board = new Board();
-            board.setSignAt(2, "O");
-            board.setSignAt(4, "X");
-            
-            System.out.println(board.toString());
             while (true) {
-                String ans = in.readUTF();
-                System.out.println(ans);
+                String chose;
+                do {
+                    out.writeUTF(" --- Menu ---\n"+
+                                 "1 - Uruchom gre\n"+
+                                 "2 - Historia gier");
+                    chose = in.readUTF();
+                } while (!chose.equals("1") && !chose.equals("2"));
 
-                
-                
-                // out.writeUTF("suma: "+suma+"\nroznica: "+roznica+"\niloczyn: "+iloczyn);                
-            }
+                if(chose.equals("1")) {
+                    board = new Board();
+        
+                    out.writeUTF("Podaj nazwe gracza 1: ");
+                    String name1 = in.readUTF();            
+                    out.writeUTF("Podaj nazwe gracza 2: ");
+                    String name2 = in.readUTF();         
+                    
+                    String sign1 = "O";
+                    String sign2 = "X";
+                    Random rand = new Random();
+                    int random = rand.nextInt(1); // 0-1
+                    if(random == 0) {
+                        sign1 = "X";
+                        sign2 = "O";
+                    }
+    
+                    Player player1 = new Player(name1, sign1);
+                    Player player2 = new Player(name2, sign2);
+        
+                    boolean player1Turn = false;
+                    if(player1.getSign().equals("X")) player1Turn = true;
+        
+                    int winner = 0; 
+                    boolean playing = true;
+                    while(playing) {
+                        String mess = board.toString()+"\n"; 
+                        if(player1Turn) {
+                            mess += "Ruch "+player1.getName()+" ("+player1.getSign()+").";
+                        } else {
+                            mess += "Ruch "+player2.getName()+" ("+player2.getSign()+").";
+                        }
+                        mess += "\nPodaj pole do wstawienia twojego znaku";
+                        out.writeUTF(mess);
+                        String res = in.readUTF();
+                        // [a-c][1-3]|[1-3][a-c]
+                        Pattern pattern = Pattern.compile("[a-c][1-3]|[1-3][a-c]", Pattern.CASE_INSENSITIVE);
+                        Matcher matcher = pattern.matcher(res);
+    
+                        while(!matcher.find() || res.length() > 2) {
+                            out.writeUTF("Niepoprawny ruchu\nPodaj pole do wstawienia");
+                            res = in.readUTF();
+                            matcher = pattern.matcher(res);
+                        }
+       
+                        pattern = Pattern.compile("[a-c][1-3]", Pattern.CASE_INSENSITIVE);
+                        matcher = pattern.matcher(res);
+                        if(!matcher.find()) res = ""+res.charAt(1)+res.charAt(0);
+    
+                        int index = 0;
+                        int number = Integer.parseInt(res.substring(1, 2));
+                        char letter = res.charAt(0);
+    
+                        if(letter == 'a') index = 0; 
+                        if(letter == 'b') index = 1;
+                        else if(letter == 'c') index = 2;
+
+                        if(number == 2) index += 3;
+                        else if(number == 3) index += 6;
+    
+                        System.out.println(index);
+
+                        String sign = player1.getSign();
+                        if(!player1Turn) sign = player2.getSign(); 
+    
+                        System.out.println(index+" "+sign);
+    
+                        if(board.getSignAt(index).equals(" ")) board.setSignAt(index, sign);
+                        else player1Turn = !player1Turn;
+    
+                        String winnerSign = null;
+                        
+                        if(board.checkWin()) {
+                            winnerSign = board.getWinnerSign();
+                            if(winnerSign.equals("-")) winner = 3;
+                            else if(player1.getSign().equals(winnerSign)) winner = 1;
+                            else if(player2.getSign().equals(winnerSign)) winner = 2;
+                            playing = false;
+                        } else player1Turn = !player1Turn;
+                    }
+                    // if(player1Turn)
+        
+                    FileWriter fw = new FileWriter(f, true);
+                    
+                    String mess = "Wygrał gracz ";
+                    String matchData = player1.getName()+" ("+player1.getSign()+") vs "+player2.getName()+" ("+player2.getSign()+")";
+                    if(winner == 3) {
+                        mess = "Remis";
+                        fw.write(matchData+" | Remis \n");
+                    } else if(winner == 1) {
+                        mess += player1.getName();
+                        fw.write(matchData+" | Zwycięzca: "+player1.getName()+"\n");
+                    } else {
+                        mess += player2.getName();
+                        fw.write(matchData+" | Zwycięzca: "+player2.getName()+"\n");
+                    }
+                    out.writeUTF(board.toString()+"\n"+mess);
+                    in.readUTF();
+                    fw.close();
+                } else if(chose.equals("2")) {
+                    Scanner sc2 = new Scanner(f);
+                    String res = "";
+                    while (sc2.hasNextLine()) {
+                        res += sc2.nextLine()+"\n";
+                    }
+                    out.writeUTF(res);
+                    in.readUTF();
+                } 
+            } 
         } catch (Exception ex) {
             System.out.println(ex.toString());
         }
