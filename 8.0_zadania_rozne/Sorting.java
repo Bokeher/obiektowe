b8e241fdea254c8e214903f7d74c5357f8313e22
Bokeher <72520841+Bokeher@users.noreply.github.com>
2023-06-02 09:19:50 +0200

    skonczone 8.0_zadania_rozne

--- 8.0_zadania_rozne/Sorting.java
+++ 8.0_zadania_rozne/Sorting.java
@@ -1,19 +1,104 @@
 public class Sorting {
-    int[] arr = null;
+    /**
+     * Metoda sortująca tablicę przy użyciu algorytmu Bubble Sort.
+     * @param array tablica do posortowania
+     */
+    public void bubbleSort(int[] array) {
+        int n = array.length;
 
-    public Sorting(int[] arr) {
-        this.arr = arr;
+        // Pętla zewnętrzna - przechodzi przez całą tablicę
+        for (int i = 0; i < n - 1; i++) {
+
+            // Pętla wewnętrzna - porównuje sąsiednie elementy i zamienia je miejscami, jeśli są w niewłaściwej kolejności
+            for (int j = 0; j < n - i - 1; j++) {
+
+                // Sprawdza, czy elementy są w niewłaściwej kolejności
+                if (array[j] > array[j + 1]) {
+                    
+                    // Zamień elementy miejscami
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+    /**
+     * Metoda sortująca tablicę przy użyciu algorytmu Quick Sort.
+     * @param array tablica do posortowania
+     * @param low   indeks początkowy
+     * @param high  indeks końcowy
+     */
+    public void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            // Dzielimy tablicę i otrzymujemy indeks podziału
+            int pivot = partition(array, low, high);
+
+            // Wywołujemy rekurencyjnie quickSort dla lewej i prawej części podziału
+            quickSort(array, low, pivot - 1);
+            quickSort(array, pivot + 1, high);
+        }
     }
 
-    public void bubbleSort() {
-        
+    /**
+     * Metoda dzieląca tablicę na podstawie wybranego elementu (pivot) i zwracająca indeks podziału.
+     * @param array tablica do podziału
+     * @param low   indeks początkowy
+     * @param high  indeks końcowy
+     * @return indeks podziału
+     */
+    public int partition(int[] array, int low, int high) {
+        // Wybieramy ostatni element jako pivot
+        int pivot = array[high];
+
+        // Indeks elementu mniejszego od pivot
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            // Jeśli aktualny element jest mniejszy lub równy pivot
+            if (array[j] <= pivot) {
+                i++;
+
+                // Zamieniamy elementy o indeksach i oraz j
+                swap(array, i, j);
+            }
+        }
+
+        // Zamieniamy pivot z elementem o indeksie i+1
+        swap(array, i + 1, high);
+
+        return i + 1;
     }
 
-    public void quickSort() {
-
+    /**
+     * Metoda pomocnicza do zamiany miejscami dwóch elementów w tablicy.
+     * @param array tablica
+     * @param i     indeks pierwszego elementu
+     * @param j     indeks drugiego elementu
+     */
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
     
-    public void insertionSort() {
+    /**
+     * Sortuje tablicę liczb przy użyciu algorytmu sortowania przez wstawianie.
+     * @param array tablica liczb do posortowania
+     */
+    public void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i]; // bieżący element do wstawienia
+            int j = i - 1;
 
+            // Przesuń elementy większe od key o jedną pozycję w prawo
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key; // Wstaw bieżący element na właściwe miejsce
+        }
     }
 }
--- 8.0_zadania_rozne/Zad4.java
+++ 8.0_zadania_rozne/Zad4.java
@@ -3,20 +3,58 @@ import java.util.Random;
 public class Zad4 {
     public static void main(String[] args) {
         // tworzy tablice
-        int[] arr = new int[1000];
+        int[] arr1 = new int[1000];
+        int[] arr2 = new int[1000];
+        int[] arr3 = new int[1000];
 
         // wypelnia tablice
-        fillArray(arr);
+        fillArray(arr1);
+        fillArray(arr2);
+        fillArray(arr3);
 
-        // TODO: all sorts
-        
+        // klasa do sortowania
+        Sorting sort = new Sorting();
+
+        // bubble sort
+        System.out.println("Bubble sort: \nTablica przed posortowaniem: "+arrayToString(arr1));
+        sort.bubbleSort(arr1);
+        System.out.println("Tablica po sortowaniu: "+arrayToString(arr1));
+
+        // quick sort
+        System.out.println("\nQuick sort: \nTablica przed posortowaniem: "+arrayToString(arr2));
+        sort.quickSort(arr2, 0, arr2.length-1);
+        System.out.println("Tablica po sortowaniu: "+arrayToString(arr2));
+
+        // insertion sort
+        System.out.println("\nInsertion sort: \nTablica przed posortowaniem: "+arrayToString(arr3));
+        sort.insertionSort(arr3);
+        System.out.println("Tablica po sortowaniu: "+arrayToString(arr3));
     }
 
+    /**
+     * Wyplenia tablice liczbami z zakresu [-100;100]
+     * @param arr tablica do wypelnienia
+     */
     public static void fillArray(int[] arr) {
         Random r = new Random();
         for (int i = 0; i < arr.length; i++) {
             // (max - min + 1) - min
             arr[i] = r.nextInt(100-(-100)+1)-100;
         }
+    }
+
+    /**
+     * Zwraca tablice jako elementy odzielone spacja w Stringu
+     * @param arr tablica do wyswietlenia
+     * @return elementy tablicy odzielona spacjami
+     */
+    public static String arrayToString(int[] arr) {
+        String res = "";
+
+        for (int i = 0; i < arr.length; i++) {
+            res += arr[i]+" ";
+        }
+
+        return res;
     }
 }
